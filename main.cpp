c448c8401b05315ad2bb7613f9a13685fd0153b8
ninkibah <ninkibah@eircom.net>
2018-02-18 22:21:39 +0000

    Added lambdas and templated functions to test code

--- main.cpp
+++ main.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
 #include "trace.h"
 
+auto inc = [](int x) {
+  TRACE;
+  Trace::printStackTrace(std::cout);
+  return x + 1;
+};
+
+auto dec = [](int x) {
+  TRACE;
+  return x + 1;
+};
+
+template<typename T>
+T plus2(T x) {
+  TRACE;
+  return x + 2;
+}
 // Some functions that call one another
 int hitchhiker() {
   TRACE;
-  Trace::printStackTrace(std::cout);
-  return 42;
+  plus2(3.0);
+  plus2(4);
+  return inc(42);
 }
+
+struct Foo {
+  Foo() {
+    TRACE;
+    Trace::printStackTrace(std::cout);
+  }
+};
 
 void f() {
   TRACE;
@@ -16,5 +40,6 @@ void f() {
 int main() {
   TRACE;
   f();
+  Foo foo;
   return 0;
 }
